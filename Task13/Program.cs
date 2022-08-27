dc4373fd336e538aa06d430736d8e23ff91384f1
unknown <volk.3894@yandex.ru>
2022-08-27 20:49:22 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = new Random().Next(-100000000, 100000000);
+//int num = -123456789;
+Console.WriteLine(num);
+if (num < 0)
+{
+    num = num * -1;
+}
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+
+if (num >= 100)
+{
+    while(num > 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
