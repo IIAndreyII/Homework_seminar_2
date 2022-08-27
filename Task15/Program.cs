8f8d4859a1d223d4da2752c77c0f5736793b6107
unknown <volk.3894@yandex.ru>
2022-08-27 21:13:02 +0300

    add Task15/

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5)
+{
+    Console.WriteLine("Это рабочий день.");
+}
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Ура! Выходной!");
+}
+
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Такого дня нет.");
+}
